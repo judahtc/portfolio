99e10cf609b247fc8198874eff1805c1636cf9e8
judahtc <jaycea247@gmail.com>
2024-03-11 17:03:19 +0200

    created the aws free trial

--- src/app/aws-free-tier/aws-free-tier.component.ts
+++ src/app/aws-free-tier/aws-free-tier.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aws-free-tier',
   standalone: true,
   imports: [],
   templateUrl: './aws-free-tier.component.html',
-  styleUrl: './aws-free-tier.component.scss'
+  styleUrl: './aws-free-tier.component.scss',
 })
-export class AwsFreeTierComponent {
-
+export class AwsFreeTierComponent implements OnInit {
+  my_email: any;
+  ngOnInit(): void {
+    this.my_email = 'jayeca247@gmail.com';
+  }
 }
